516eec2318e3e8db8b5d4e551b98fadd8e85a545
abarcadavid <davidabarca.swe@protonmail.com>
2023-06-26 21:35:19 -0700

    Setup worker client for snarkyjs

--- ui/src/pages/_app.tsx
+++ ui/src/pages/_app.tsx
@@ -1,20 +1,83 @@
 import '@/styles/globals.css'
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import ZkappWorkerClient from './zkAppWorkerClient';
 import { PublicKey, Field } from 'snarkyjs';
 
 export default function App() {
   let [state, setState] = useState({
-    zkAppWorkerClient: null as null | ZkappWorkerClient,
+    zkappWorkerClient: null as null | ZkappWorkerClient,
     hasWallet: null as null | boolean,
     hasBeenSetup: false,
     accountExists: false,
     currentNum: null as null | Field,
     publicKey: null as null | PublicKey,
-    zkAppPublicKey: null as null | PublicKey,
+    zkappPublicKey: null as null | PublicKey,
     creatingTransaction: false,
   });
+
+  // -------------------------------------------------------
+  // Do Setup
+
+  useEffect(() => {
+    (async () => {
+      if (!state.hasBeenSetup) {
+        const zkappWorkerClient = new ZkappWorkerClient();
+
+        console.log('Loading SnarkyJS...');
+        await zkappWorkerClient.loadSnarkyJS();
+        console.log('done');
+
+        await zkappWorkerClient.setActiveInstanceToBerkeley();
+
+        const mina = (window as any).mina;
+
+        if (mina == null) {
+          setState({ ...state, hasWallet: false });
+          return;
+        }
+
+        const publicKeyBase58: string = (await mina.requestAccounts())[0];
+        const publicKey = PublicKey.fromBase58(publicKeyBase58);
+
+        console.log('using key', publicKey.toBase58());
+
+        console.log('checking if account exists...');
+        const res = await zkappWorkerClient.fetchAccount({
+          publicKey: publicKey!,
+        });
+        const accountExists = res.error == null;
+
+        await zkappWorkerClient.loadContract();
+
+        console.log('compiling zkApp');
+        await zkappWorkerClient.compileContract();
+        console.log('zkApp compiled');
+
+        const zkappPublicKey = PublicKey.fromBase58(
+          'B62qohp4zipB5jHp2r8tZyCX1j65H37aQjn4vh9uyHskn3jfnSxbeRu'
+        );
+
+        await zkappWorkerClient.initZkappInstance(zkappPublicKey);
+
+        console.log('getting zkApp state...');
+        await zkappWorkerClient.fetchAccount({ publicKey: zkappPublicKey });
+        const currentNum = await zkappWorkerClient.getNum();
+        console.log('current state:', currentNum.toString());
+
+        setState({
+          ...state,
+          zkappWorkerClient,
+          hasWallet: true,
+          hasBeenSetup: true,
+          publicKey,
+          zkappPublicKey,
+          accountExists,
+          currentNum,
+        });
+      }
+    })();
+  }, []);
 
   return <div />
 }
